6ef05cb3dd610719bd699c6426b657e41ca9ab1b
NephiImo <imonephi@gmail.com>
2026-07-06 12:27:24 +0100

    Refactor contacts routes to use controller methods for improved organization and maintainability

---
+++ contacts-api/controllers/contacts.js
@@ -0,0 +1,58 @@
+const { ObjectId } = require("mongodb");
+const mongodb = require("../db/connect");
+
+const getAllContacts = async (req, res) => {
+  try {
+    const db = mongodb.getDb();
+
+    const contacts = await db
+      .collection("contacts")
+      .find()
+      .toArray();
+
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(contacts);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error retrieving contacts",
+      error: error.message
+    });
+  }
+};
+
+const getSingleContact = async (req, res) => {
+  try {
+    const contactId = req.params.id;
+
+    if (!ObjectId.isValid(contactId)) {
+      return res.status(400).json({
+        message: "Invalid contact ID"
+      });
+    }
+
+    const db = mongodb.getDb();
+
+    const contact = await db
+      .collection("contacts")
+      .findOne({ _id: new ObjectId(contactId) });
+
+    if (!contact) {
+      return res.status(404).json({
+        message: "Contact not found"
+      });
+    }
+
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(contact);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error retrieving contact",
+      error: error.message
+    });
+  }
+};
+
+module.exports = {
+  getAllContacts,
+  getSingleContact
+};
