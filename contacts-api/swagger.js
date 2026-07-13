7035f7a8d4bc6a0086ceae4d44979156da43c8aa
NephiImo <imonephi@gmail.com>
2026-07-13 17:04:42 +0100

    Update Swagger configuration for production environment and enhance API documentation

--- contacts-api/swagger.js
+++ contacts-api/swagger.js
@@ -3,10 +3,29 @@ const swaggerAutogen = require("swagger-autogen")();
 const doc = {
   info: {
     title: "Contacts API",
-    description: "API documentation for CSE341 Contacts Project"
+    description: "API documentation for CSE341 Contacts Project",
   },
-  host: "localhost:8080",
-  schemes: ["http"]
+
+  host: "cse341-ujj4.onrender.com",
+  basePath: "/contacts",
+  schemes: ["https"],
+  
+  tags: [
+    {
+      name: "Contacts",
+      description: "Contact management endpoints",
+    },
+  ],
+
+  definitions: {
+    Contact: {
+      firstName: "Nephi",
+      lastName: "Imo",
+      email: "nephi@example.com",
+      favoriteColor: "Blue",
+      birthday: "2003-01-01",
+    },
+  },
 };
 
 const outputFile = "./swagger.json";
